0c10b50de0d9514d61447cd31af4e796589e49eb
mar-ven <marcovenere99@gmail.com>
2022-07-31 12:36:40 +0200

    changes to gitignore

--- sysroot/usr/include/kernel/keyboard.h
+++
@@ -1,6 +0,0 @@
-#ifndef ARCH_I386_KEYBOARD_H
-#define ARCH_I386_KEYBOARD_H
-
-void init_keyboard();
-
-#endif
--- sysroot/usr/include/kernel/timer.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARCH_I386_TIMER_H
-#define ARCH_I386_TIMER_H
-
-#include <stdint.h>
-
-void init_timer(uint32_t frequency);
-
-#endif
